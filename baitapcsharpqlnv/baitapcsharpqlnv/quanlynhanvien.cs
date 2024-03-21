97108bcfffcc7baf8f32d2eb935bd986a0a64351
Zon_206XLag <56264968+anhhackta@users.noreply.github.com>
2024-03-21 16:41:04 +0700

    sau big update

--- baitapcsharpqlnv/baitapcsharpqlnv/quanlynhanvien.cs
+++ baitapcsharpqlnv/baitapcsharpqlnv/quanlynhanvien.cs
@@ -14,6 +14,18 @@ namespace baitapcsharpqlnv
         {
             dsnhanvien = new Dictionary<String, NHANVIEN>();
         }
+        public void Nhap()
+        {
+            //char c = 'y';
+            //while (c == 'y' || c == 'Y')
+            //{
+            //    NHANVIEN nv = new NHANVIEN();
+            //    nv.Nhap();
+            //    danhsachnhanvien.Add(nv.MANV, nv);
+            //    Console.WriteLine("nhap y de tiep tuc , bat ky de ket thuc ! ");
+            //    c = Convert.ToChar(Console.ReadLine());
+            //}
+        }
 
     }
 }
